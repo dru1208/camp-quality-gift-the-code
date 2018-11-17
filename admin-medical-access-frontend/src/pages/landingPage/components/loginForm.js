e002f9980350f2af390c4207c087ef86555f517b
Andrew Hsieh <andrewhsieh1208@gmail.com>
2018-11-17 03:47:52 -0500

    react version

---
+++ admin-medical-access-frontend/src/pages/landingPage/components/loginForm.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const LoginForm = () => (
+  <form>
+    <label for="username">Username:</label>
+    <input type="text" name="username" />
+      <br />
+    <label for="password">Password:</label>
+    <input type="password" name="password" />
+      <br />
+    <input type="submit" value="Login"/>
+  </form>
+)
+
+export default LoginForm
---
+++ admin-medical-access-frontend/src/pages/landingPage/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import LoginForm from "./components/loginForm"
+
+const Home = props => (
+  <div>
+    <div>Welcome to the Medical Administration Portal</div>
+    <LoginForm />
+  </div>
+)
+
+
+export default Home
