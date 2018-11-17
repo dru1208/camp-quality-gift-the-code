182fdf33e8acc209b98e71e9f7b38676be5478d5
Wiljay <wiljay.flores@gmail.com>
2018-11-16 23:48:20 -0500

    created iOS project

---
+++ parent-access-frontend-mobile/camp-quality-parentApp-iOS/camp-quality-parentApp-iOSTests/camp_quality_parentApp_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  camp_quality_parentApp_iOSTests.swift
+//  camp-quality-parentApp-iOSTests
+//
+//  Created by Wiljay Flores on 2018-11-16.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+import XCTest
+@testable import camp_quality_parentApp_iOS
+
+class camp_quality_parentApp_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
