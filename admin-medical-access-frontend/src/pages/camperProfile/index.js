f7108b1319129f94622f423b70b416b8a77a866b
KPalos <kyla.palos@gmail.com>
2018-11-18 07:25:54 -0500

    fixed variable naming for consistency

--- admin-medical-access-frontend/src/pages/camperProfile/index.js
+++ admin-medical-access-frontend/src/pages/camperProfile/index.js
@@ -24,7 +24,7 @@ class CamperProfile extends Component {
       primaryDiagnosis: 'Cancer',
       secondaryDiagnosis: 'Leukemia',
       allergy: 'Peanut, Coffee, Milk',
-      emergencyContact: {
+      parentInfo: {
         firstName: 'Jane',
         lastName: 'Doe',
         relationship: 'mom',
@@ -58,39 +58,50 @@ class CamperProfile extends Component {
       primaryDiagnosis,
       secondaryDiagnosis,
       allergy,
-      emergencyContact,
+      parentInfo,
       treatment } = this.state.info;
-    const { loading, camperID } = this.state
-    return loading ? <h3>Loading</h3> :
-        <div className="camper-profile">
-          <div className="camper-profile--basic">
-            <h3>Basic Profile</h3>
-            <p>
-              Name: {firstName} {lastName}
-            </p>
-            <p>Primary Diagnosis: {primaryDiagnosis}</p>
-            <p>Secondary Diagnosis: {secondaryDiagnosis}</p>
-            <p>Is over-the-counter medicine allowed? {otcPermitted ? "Yes" : "No"}</p>
-            <p>Allergy: {allergy}</p>
-            <p>Notes: {notes}</p>
-          </div>
+    const { loading } = this.state
+    return loading ? <h3>Loading</h3> : <div className="camper-profile">
+        <div className="camper-profile--basic">
+          <h3>Basic Profile</h3>
+          <p>
+            Name: {firstName} {lastName}
+          </p>
+          <p>Primary Diagnosis: {primaryDiagnosis}</p>
+          <p>Secondary Diagnosis: {secondaryDiagnosis}</p>
+          <p>
+            Is over-the-counter medicine allowed?{" "}
+            {otcPermitted ? "Yes" : "No"}
+          </p>
+          <p>Allergy: {allergy}</p>
+          <p>Notes: {notes}</p>
+        </div>
 
-          <div className="camper-profile--emergency">
-            <h3>Emergency Contact Info:</h3>
-            <p>Name: {emergencyContact.firstName} {emergencyContact.lastName}</p>
-            <p>Relationship: {emergencyContact.relationship}</p>
-            <p>Email: {emergencyContact.email}</p>
-            <p>Phone: {emergencyContact.phone}</p>
-          </div>
+        <div className="camper-profile--emergency">
+          <h3>Emergency Contact Info:</h3>
+          <p>
+            Name: {parentInfo.firstName} {parentInfo.lastName}
+          </p>
+          <p>Relationship: {parentInfo.relationship}</p>
+          <p>Email: {parentInfo.email}</p>
+          <p>Phone: {parentInfo.phone}</p>
+        </div>
 
-          <div className="camper-profile--treatment">
-            <h3>Treatment</h3>
-            <p>Medication: {treatment.medication} {treatment.dosage}</p>
-            <p>Date and Time: {treatment.date} {treatment.time}</p>
-            <p>Has been administered? {treatment.beenAdministered ? "Yes" : "No"}</p>
-            <p>Details: {treatment.details}</p>
-          </div>
-        </div>;
+        <div className="camper-profile--treatment">
+          <h3>Treatment</h3>
+          <p>
+            Medication: {treatment.medication} {treatment.dosage}
+          </p>
+          <p>
+            Date and Time: {treatment.date} {treatment.time}
+          </p>
+          <p>
+            Has been administered?{" "}
+            {treatment.beenAdministered ? "Yes" : "No"}
+          </p>
+          <p>Details: {treatment.details}</p>
+        </div>
+      </div>;
   }
 }
 
